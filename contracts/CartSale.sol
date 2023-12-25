9a803680372c9bcc0a0e18bcb31be0c0c52bef5e
mayank jain <mayankjain1394@gmail.com>
2023-12-25 22:33:23 +0530

    WIP: Added order data support in CartSale

--- contracts/CartSale.sol
+++ contracts/CartSale.sol
@@ -2,7 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract CartSale {
-    address public sellerAccount = 0x8771B888Fe2486F3a2874703cb09f245a3e4f3e0;
+    address sellerAccount = 0x8771B888Fe2486F3a2874703cb09f245a3e4f3e0;
+
+    struct Order {
+        uint orderId;
+        uint amount;
+        uint items;
+        uint timestamp;
+    }
+
+    struct Orders {
+        uint userId;
+        uint numOrders;
+        // mapping(uint => Order) orders;
+    }
+
+    mapping(address => Orders) public usersOrders;
 
     event TransactionComplete(
         address indexed buyer,
@@ -12,7 +27,12 @@ contract CartSale {
 
     constructor() {}
 
-    function purchaseTokens(uint256 _amount) public payable {
+    function purchaseCoffee(
+        uint256 _amount,
+        uint orderId,
+        uint userId,
+        uint items
+    ) public payable {
         uint256 amountInWei = _amount * 1e18;
 
         require(
@@ -24,7 +44,16 @@ contract CartSale {
             "Insufficient account balance"
         );
 
+        // Order memory newOrder = Order(orderId, _amount, items, block.timestamp);
+        Orders memory newOrders = Orders(0, userId);
+        usersOrders[msg.sender] = newOrders;
+        // usersOrders[msg.sender].userId = userId;
+        // usersOrders[msg.sender].orders[
+        //     usersOrders[msg.sender].numOrders
+        // ] = newOrder;
+
         payable(sellerAccount).transfer(amountInWei);
+
         emit TransactionComplete(msg.sender, sellerAccount, amountInWei);
     }
 }
