138ff2f1cd8871fcddb92d9274c28cc909111812
mayank jain <mayankjain1394@gmail.com>
2023-12-29 20:42:34 +0530

    Project restructure

--- src/hooks/useEthContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import EthContext from "../context/EthContext";
-
-function useEthContext() {
-	return useContext(EthContext);
-}
-
-export default useEthContext;
