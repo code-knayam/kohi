f96de7b6692f72d7bce8b40fed0f5e473b0c101b
mayank jain <mayankjain1394@gmail.com>
2023-12-30 20:08:19 +0530

    Added first draft for getItems and createOrder API

--- client/functions/index.js
+++ client/functions/index.js
@@ -7,13 +7,63 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const { onRequest } = require("firebase-functions/v2/https");
+const { logger } = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firebaseConfig = {
+	apiKey: "AIzaSyCYG-RL1KAU_nV8dTMcppQaNPYBv2pHX64",
+	authDomain: "kohi-fcd46.firebaseapp.com",
+	databaseURL: "https://kohi-fcd46-default-rtdb.firebaseio.com",
+	projectId: "kohi-fcd46",
+	storageBucket: "kohi-fcd46.appspot.com",
+	messagingSenderId: "398853462488",
+	appId: "1:398853462488:web:33c54f82e28ac2cce23aad",
+	measurementId: "G-JHLS7RLKE1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+exports.createOrder = onRequest(async (req, res) => {
+	const data = req.query.payload;
+
+	const db = await getFirestore(app);
+	const newOrderRef = db.collection("orders").doc();
+
+	await newOrderRef.set(data);
+
+	res.json({ result: "message with ID: added" });
+});
+
+exports.getItems = onRequest(async (req, res) => {
+	const db = await getFirestore(app);
+	const ordersRef = db.collection("items");
+
+	const querySnapshot = await ordersRef.get();
+
+	const items = [];
+
+	querySnapshot.forEach((doc) => {
+		items.push({ id: doc.id, ...doc.data() });
+	});
+
+	res.json(items);
+});
+
+exports.setItems = onRequest(async (req, res) => {
+	const db = await getFirestore(app);
+	const promises = [];
+	const menuItems = res.query.payload;
+	menuItems.menu.forEach((item) => {
+		const newItemRef = db.collection("items").doc();
+
+		promises.push(newItemRef.set(item));
+	});
+
+	await Promise.all(promises);
+
+	res.json("items added");
+});
